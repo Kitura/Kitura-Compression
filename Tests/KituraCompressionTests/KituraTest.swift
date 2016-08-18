54296f39442ff3255fe96596e0e32bce868ba931
Shmuel Kallner <kallner@il.ibm.com>
2016-08-18 13:45:21 +0300

    IBM-Swift/Kitura#675 Swift language changes and migration from KituraSys.Queue to Dispatch

--- Tests/KituraCompressionTests/KituraTest.swift
+++ Tests/KituraCompressionTests/KituraTest.swift
@@ -17,9 +17,9 @@
 import XCTest
 
 @testable import KituraNet
-@testable import KituraSys
 
 import Foundation
+import Dispatch
 
 
 protocol KituraTest {
@@ -33,15 +33,15 @@ extension KituraTest {
   //       sleep(10)
     }
 
-    func performServerTest(_ router: ServerDelegate, asyncTasks: (expectation: XCTestExpectation) -> Void...) {
+    func performServerTest(_ router: ServerDelegate, asyncTasks: @escaping (XCTestExpectation) -> Void...) {
         let server = setupServer(port: 8090, delegate: router)
         sleep(10)
-        let requestQueue = Queue(type: .serial)
+        let requestQueue = DispatchQueue(label: "Request queue")
 
         for (index, asyncTask) in asyncTasks.enumerated() {
             let expectation = self.expectation(index)
-            requestQueue.enqueueAsynchronously {
-                asyncTask(expectation: expectation)
+            requestQueue.async {
+                asyncTask(expectation)
             }
         }
 
@@ -75,7 +75,7 @@ extension KituraTest {
 
 extension XCTestCase: KituraTest {
     func expectation(_ index: Int) -> XCTestExpectation {
-        let expectationDescription = "\(self.dynamicType)-\(index)"
+        let expectationDescription = "\(type(of: self))-\(index)"
         return self.expectation(description: expectationDescription)
     }
 
