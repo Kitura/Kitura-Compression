b501257ce752a90329574b7a6d65ecdb31fca5db
Shmuel Kallner <kallner@il.ibm.com>
2016-11-03 15:08:04 +0200

    Upgrade to Swift 3.0.1 (#22)
    * IBM-Swift/Kitura#832 Upgraded to Swift 3.0.1
    
    * IBM-Swift/Kitura#832 Updated to lated versions of dependencies

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "KituraCompression",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
+            .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
             .Package(url: "https://github.com/IBM-Swift/CZlib.git", majorVersion: 0, minor: 1),
             ]
 )
