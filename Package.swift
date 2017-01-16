cbed94c91458d4a5f42b4a8f6b2b9855f5de2fb4
Navneet Gupta <navneet@us.ibm.com>
2017-01-16 14:19:02 -0600

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "KituraCompression",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
+            .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 5),
             .Package(url: "https://github.com/IBM-Swift/CZlib.git", majorVersion: 0, minor: 1),
             ]
 )
